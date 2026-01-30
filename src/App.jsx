558bd3dd6f9567fd0142e9944fc2ee1dd78d35eb
Eddie Yeung <eycs0317@gmail.com>
2026-01-30 10:42:59 -0800

    add button to switch game

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import Matching from "./Matching.jsx";
 import Puzzle from "./Puzzle.jsx";
-
+import { useState } from "react";
+import "./App.css";
 function App() {
+  const [switchGame, setSwitchGame] = useState(false);
   return (
     <>
       <p className='text-6xl font-bold text-center p-4'>Matching Game</p>
-      {/* <Matching /> */}
-      <Puzzle />
+      <div className='flex justify-center space-x-4 mb-4'>
+        <button onClick={() => setSwitchGame(!switchGame)}>
+          Matching Game
+        </button>{" "}
+        {/* <Matching /> */}
+        <button onClick={() => setSwitchGame(!switchGame)}>Puzzle Game</button>
+      </div>
+
+      {switchGame ? <Matching /> : <Puzzle />}
     </>
   );
 }
